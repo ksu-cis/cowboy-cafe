3b10e13fd3726cb1d6bef51d66c2238fe75af82b
amandajoelleyoung <nhbean@ksu.edu>
2020-01-20 22:34:17 -0600

    fixed price and calories for Texas Triple

--- DataTests/UnitTests/TexasTripleBurgerTest.cs
+++ DataTests/UnitTests/TexasTripleBurgerTest.cs
@@ -11,14 +11,14 @@ namespace CowboyCafe.DataTests
         public void DefaultPriceShouldBeCorrect()
         {
             var burger = new TexasTripleBurger();
-            Assert.Equal(4.50, burger.Price);
+            Assert.Equal(6.45, burger.Price);
         }
 
         [Fact]
         public void DefaultCaloriesShouldBeCorrect()
         {
             var burger = new TexasTripleBurger();
-            Assert.Equal<uint>(288, burger.Calories);
+            Assert.Equal<uint>(698, burger.Calories);
         }
 
         [Fact]
