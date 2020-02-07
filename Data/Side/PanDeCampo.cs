d15a16fe68561ecb38fe0f586971389b4936d92b
ijp <ijp@CIS-RDS4>
2020-02-07 16:13:03 -0600

    made the corn dogs and pandecamp0s

---
+++ Data/Side/PanDeCampo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class PanDeCampo : Side
+    {
+        /// <summary>
+        /// initializes the values for the prices of various sizes
+        /// </summary>
+        public PanDeCampo()
+        {
+            prices = new double[] { 1.59, 1.79, 1.99 };
+            calories = new uint[] { 227, 269, 367 };
+        }
+    }
+}
