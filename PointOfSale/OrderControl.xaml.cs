c244cde953e95702af38fc1dab3a8fd2a8211a74
ijp <ijp@CIS-RDS4>
2020-02-24 13:00:20 -0600

    i think i recovered my work mostly

--- PointOfSale/OrderControl.xaml.cs
+++ PointOfSale/OrderControl.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,11 +14,11 @@ using System.Windows.Shapes;
 namespace PointOfSale
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for OrderControl.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class OrderControl : UserControl
     {
-        public MainWindow()
+        public OrderControl()
         {
             InitializeComponent();
         }
