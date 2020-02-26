64b6aada8c3c81ab05b19e5764aab912fa6ca822
Isaac Peterson <ijp@ksu.edu>
2020-02-26 16:45:29 -0600

    order tests

--- Data/Order.cs
+++ Data/Order.cs
@@ -1,11 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
 
 namespace CowboyCafe.Data
 {
-    class Order
+    public class Order : INotifyPropertyChanged
     {
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        List<IOrderItem> items = new List<IOrderItem>();
+
+        /// <summary>
+        /// list of all items in the order
+        /// </summary>
+        public List<IOrderItem> Items { get => items; }
+
+        /// <summary>
+        /// total price of all items on the order
+        /// </summary>
+        public double Subtotal
+        {
+            get
+            {
+                double subtotal = 0;
+                foreach(IOrderItem item in items)
+                {
+                    subtotal += item.Price;
+                }
+                return subtotal;
+            }
+        }
+
+        /// <summary>
+        /// adds an item to the order
+        /// </summary>
+        /// <param name="item"></param>
+        public void Add(IOrderItem item)
+        {
+            items.Add(item);
+        }
+
+        /// <summary>
+        /// removes an item from the order
+        /// </summary>
+        /// <param name="item"></param>
+        public void Remove(IOrderItem item)
+        {
+            items.Remove(item);
+        }
     }
 }
--- DataTests/UnitTests/OrderTest.cs
+++ DataTests/UnitTests/OrderTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
+using CowboyCafe.Data;
 
 namespace CowboyCafe.DataTests.UnitTests
 {
@@ -23,7 +24,46 @@ namespace CowboyCafe.DataTests.UnitTests
 
 
         //should be able to get an inumeration of the items
+        [Fact]
+        public void ShouldGiveAnInumerationOfItems()
+        {
+            var order = new Order();
+            order.Add(new AngryChicken());
+        }
+
 
         //subtotal should be the sum of all item prices
+        [Fact]
+        public void ShouldHaveCorrectSubtotal()
+        {
+
+        }
+
+        [Theory]
+        [InlineData("Price")]
+        [InlineData)"Items")]
+        public void AddingAnItemShouldTriggerPropertyChanged(string propertyName)
+        {
+            var order = new Order();
+            var item = new MockOrderItem();
+            Assert.PropertyChanged(order, propertyName, () =>
+            {
+                order.Add(item);
+            });
+        }
+
+        [Theory]
+        [InlineData("Price")]
+        [InlineData)"Items")]
+        public void RemovingAnItemShouldTriggerPropertyChanged(string propertyName)
+        {
+            var order = new Order();
+            var item = new MockOrderItem();
+            order.Add(item);
+            Assert.PropertyChanged(order, propertyName, () =>
+            {
+                order.Remove(item);
+            });
+        }
     }
 }
