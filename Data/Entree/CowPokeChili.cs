d03c0800da2e308a7b12cd7fa8ad81d3d6223982
Isaac Peterson <ijp@ksu.edu>
2020-02-26 15:58:11 -0600

    somehow for some goddamed reason cowpokechilli was deleated, but I got it back

---
+++ Data/Entree/CowPokeChili.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// A class representing the Cowpoke Chili entree
+    /// </summary>
+    public class CowpokeChili : Entree
+    {
+        private bool cheese = true;
+        /// <summary>
+        /// If the chili is topped with cheese
+        /// </summary>
+        public bool Cheese
+        {
+            get { return cheese; }
+            set { cheese = value; }
+        }
+
+        private bool sourCream = true;
+        /// <summary>
+        /// If the chili is topped with sour cream
+        /// </summary>
+        public bool SourCream
+        {
+            get { return sourCream; }
+            set { sourCream = value; }
+        }
+
+        private bool greenOnions = true;
+        /// <summary>
+        /// If the chili is topped with green onions
+        /// </summary>
+        public bool GreenOnions
+        {
+            get { return greenOnions; }
+            set { greenOnions = value; }
+        }
+
+        private bool tortillaStrips = true;
+        /// <summary>
+        /// If the chili is topped with tortilla strips
+        /// </summary>
+        public bool TortillaStrips
+        {
+            get { return tortillaStrips; }
+            set { tortillaStrips = value; }
+        }
+
+        /// <summary>
+        /// Special instructions for the preparation of the chili
+        /// </summary>
+        public override List<string> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<string>();
+
+                if (!cheese) instructions.Add("hold cheese");
+                if (!sourCream) instructions.Add("hold sour cream");
+                if (!greenOnions) instructions.Add("hold green onions");
+                if (!tortillaStrips) instructions.Add("hold tortilla strips");
+
+                return instructions;
+            }
+        }
+
+        /// <summary>
+        /// constructor that sets the price and calorie values
+        /// </summary>
+        public CowpokeChili()
+        {
+            price = 6.10;
+            calories = 171;
+        }
+
+        /// <summary>
+        /// returns a text representation of the entree
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return "Cowpoke Chilli";
+        }
+    }
+}
