8feab42742f88aa13091d0e2b468ae44c6c9cb7a
Isaac Peterson <ijp@ksu.edu>
2020-02-26 15:49:08 -0600

    added test class for order class

---
+++ DataTests/UnitTests/OrderTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit
+
+namespace CowboyCafe.DataTests.UnitTests
+{
+    public class OrderTest
+    {
+        //should be able to add items
+
+        //should be able to remove items
+
+        //should be able to get an inumeration of the items
+
+        //subtotal should be the sum of all item prices
+    }
+}
